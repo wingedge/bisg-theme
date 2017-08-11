6df9e4c9c32f47089423f9fd6119d45bc31ca59e
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-11 19:28:08 +0800

    ako gi au ang video

--- section/frontpage-video.php
+++ section/frontpage-video.php
@@ -4,10 +4,10 @@
 			<a href="<?php the_permalink();?>" title="<?php the_title();?>">
 			<?php if ( !has_post_thumbnail() ): ?><?php echo bi_get_post_image();?>
 			<?php else:?><?php the_post_thumbnail();?><?php endif;?>
-				<!--<div class="featured-video-title">
+				<div class="featured-video-title1">
 					<span><?php the_title();?></span>
 					<span class="icon-play"><i class="fa fa-play-circle-o" aria-hidden="true"></i></span>						
-				</div>-->
+				</div>
 			</a>
 		</div>
 	</div>
