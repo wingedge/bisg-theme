736de56ead5c40a86e16713ac610cf74c060188b
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-15 16:37:27 +0800

    editing sidebar
    editing sidebar

--- sidebar.php
+++ sidebar.php
@@ -2,33 +2,25 @@
 <?php if ( is_active_sidebar( 'sidebar-main' ) ) : ?>
 <?php dynamic_sidebar( 'sidebar-main' ); ?>
 <?php else:?>
-<div class="sidebar">
-	<div class="row">
-    	<div class="col-md-12">
-        	<div class="find-exact-what-you-need">
-            	<h1>Find Exactly What You Need!</h1>
-            </div>
-        </div>
-        <div class="col-md-12">
-        	<div>
-            	<img src="<?php bloginfo( 'template_url' ); ?>/img/Travel-Insider-Banner.png"/>
-            </div>
-        </div>
-        <div class="col-md-12">
-        	<div>
-            	<img src="<?php bloginfo( 'template_url' ); ?>/img/Award-winners-1.jpeg"/>
-            </div>
-        </div>
-        <div class="col-md-12">
-        	<div class="featured-video-container slick-slider-one" id="sidebar-product-carousel">
-            	<?php bi_display_product_sidebar();?>
-            </div>
-        </div>
-        <div class="col-md-12">
-        	<div>
-            	<img src="<?php bloginfo( 'template_url' ); ?>/img/BI-membership-banner-awards-280x505.jpg"/>
-            </div>
-        </div>
+<div class="row">
+  <div class="col-md-12 s-widget">
+    <div class="find-exact-what-you-need">
+      <h1>Find Exactly What You Need!</h1>
     </div>
+  </div>
+  <div class="col-md-12 s-widget">
+    <div> <img src="<?php bloginfo( 'template_url' ); ?>/img/Travel-Insider-Banner.png"/> </div>
+  </div>
+  <div class="col-md-12 s-widget">
+    <div> <img src="<?php bloginfo( 'template_url' ); ?>/img/Award-winners-1.jpeg"/> </div>
+  </div>
+  <div class="col-md-12 s-widget">
+    <div class="featured-video-container slick-slider-one" id="sidebar-product-carousel">
+      <?php bi_display_product_sidebar();?>
+    </div>
+  </div>
+  <div class="col-md-12 s-widget">
+    <div> <img src="<?php bloginfo( 'template_url' ); ?>/img/BI-membership-banner-awards-280x505.jpg"/> </div>
+  </div>
 </div>
 <?php endif; // end primary widget area ?>
