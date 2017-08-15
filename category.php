fa08e65cfc87bd8c624f637ccc74b33b989c4e67
Wingedge <moreno.francis@gmail.com>
2017-08-15 19:05:24 +0800

    category fix
    showing of products

--- category.php
+++ category.php
@@ -6,10 +6,10 @@
 	</div>
 	<div class="row">
 		<div id="category-main-slider" class="category-main-slider col-md-5">
-			<h2><?php ucwords(single_cat_title());?></h2>
+			<h3><?php ucwords(single_cat_title());?></h3>
 		</div>
 		<div id="category-articles" class="category-articles col-md-4">
-			<h2>Latest in <?php ucwords(single_cat_title());?></h2>
+			<h3>Latest in <?php ucwords(single_cat_title());?></h3>
 		</div>
 		<div id="category-articles" class="category-articles col-md-3">
 			<?php get_sidebar('category');?>
@@ -17,30 +17,43 @@
 	</div>
 	<div class="row">
 		<div class="col-md-6 category-videos">
-			<h2><?php ucwords(single_cat_title());?> Videos</h2>
+			<h3><?php ucwords(single_cat_title());?> Videos</h3>
 			<div class="category-video-containers row">
-			<?php /*set arguments */
-				$videos = array(
-				    'posts_per_page' 	=> 3,
-				    'category_name' 	=> NULL, //reset
-				    'file_template'	 	=> 'section/category-video.php',
-				    'tax_query' 		=> array(
-									        array(
-									            'taxonomy' => 'category',
-									            'field' => 'slug',
-									            'terms' => 'makeup-videos',
-									            //'operator' => 'AND'
-									        )
-				    )
-				);
-			?>
-			<?php bi_display_popular_videos($videos);?>
+				<?php /*set arguments */
+					$videosArgs = array(
+					    'posts_per_page' 	=> 3,
+					    'category_name' 	=> NULL, //reset
+					    'file_template'	 	=> 'section/category-video.php',
+					    'tax_query' 		=> array( array(
+										            'taxonomy' => 'category',
+										            'field' => 'slug',
+										            'terms' => 'makeup-videos',
+										            //'operator' => 'AND'
+						))
+					);
+				?>
+				<?php bi_display_popular_videos($videosArgs);?>
 			</div>
 			
 		</div>
 		<div class="col-md-6">
-			<?php ucwords(single_cat_title());?> Products
-			products here
+			<h3><?php ucwords(single_cat_title());?> Products</h3>			
+			<div class="category-video-containers row">
+			<?php /*set arguments */
+				$productArgs = array(
+				    'posts_per_page' 	=> 10,
+					'post_type'			=> 'product',			    
+				    'category_name' 	=> NULL, //reset
+				    'file_template'	 	=> 'section/category-products.php',
+				    'tax_query' 		=> array( array(
+									            'taxonomy' => 'category',
+									            'field' => 'slug',
+									            'terms' => 'makeup-videos',
+									            //'operator' => 'AND'
+					))
+				);
+			?>
+			<?php bi_display_products($productArgs);?>
 		</div>
 	</div>
 </div>
