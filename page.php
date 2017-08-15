16771e99f43e75532d97e356ccc71de2dd55c9ec
Wingedge <moreno.francis@gmail.com>
2017-08-15 15:09:55 +0800

    test auto deploy

--- page.php
+++ page.php
@@ -5,6 +5,7 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="entry-content">
+					deep
 					<?php the_content(); ?>
 				</div>
 			</div>
