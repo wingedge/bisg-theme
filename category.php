b7d6787091d1378ff431d39255130d152c772c28
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-16 16:50:27 +0800

    fixing breadcrumbs
    fixing breadcrumbs

--- category.php
+++ category.php
@@ -1,32 +1,45 @@
 <?php get_header(); ?>
 <style>
-.category-page .slick-slide img,
-.category-page .recent-article-row img {
-    height: auto !important;
+.category-page .slick-slide img, .category-page .recent-article-row img {
+	height: auto !important;
+}
+.breadcrumbs {
+	margin: 0;
+	padding: 0;
+}
+.breadcrumbs li {
+	display: inline-block;
+	list-style: outside none none;
+	margin: 0;
+	padding: 5px;
 }
 </style>
-<div class="main-content container category-page">
-	<div class="row breadcrumbs-row">
-		<?php bi_breadcrumbs();?>
-	</div>
-	<div class="row">
-		<div id="category-main-slider" class="category-main-slider col-md-5">
-			<h3><?php ucwords(single_cat_title());?></h3>
-
-			<?php 
+<div class="category-page">
+  <div class="row breadcrumbs-row">
+    <?php bi_breadcrumbs();?>
+  </div>
+  <div class="main-content container">
+    <div class="row">
+      <div id="category-main-slider" class="category-main-slider col-md-5">
+        <h3>
+          <?php ucwords(single_cat_title());?>
+        </h3>
+        <?php 
 				$catArticleArgsNew = array(
 					'category_name' => single_cat_title(null,false),
 					'post_per_page'	=> 3,
 					'file_template' => 'section/category-article-slider.php',					
 				);
 			?>
-			<div class="category-articles-container slick-slider-one">			
-				<?php bi_display_brand($catArticleArgsNew);?>
-			</div>
-		</div>
-		<div id="category-articles" class="category-articles col-md-4">
-			<h3>Latest in <?php ucwords(single_cat_title());?></h3>
-			<?php 
+        <div class="category-articles-container slick-slider-one">
+          <?php bi_display_brand($catArticleArgsNew);?>
+        </div>
+      </div>
+      <div id="category-articles" class="category-articles col-md-4">
+        <h3>Latest in
+          <?php ucwords(single_cat_title());?>
+        </h3>
+        <?php 
 				$catArticleArgs = array(
 					'category_name' => single_cat_title(null,false),
 					'post_per_page'	=> 8,
@@ -34,19 +47,21 @@
 					'offset' => 3, // skips first 3 since its displayed earlier
 				);
 			?>
-			<div class="category-articles-container">			
-				<?php bi_display_brand($catArticleArgs);?>
-			</div>
-		</div>
-		<div id="all-articles" class="all-articles col-md-3">
-			<?php get_sidebar('category');?>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6 category-videos">
-			<h3><?php ucwords(single_cat_title());?> Videos</h3>
-			<div class="category-video-containers row">
-				<?php /*set arguments */
+        <div class="category-articles-container">
+          <?php bi_display_brand($catArticleArgs);?>
+        </div>
+      </div>
+      <div id="all-articles" class="all-articles col-md-3">
+        <?php get_sidebar('category');?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 category-videos">
+        <h3>
+          <?php ucwords(single_cat_title());?>
+          Videos</h3>
+        <div class="category-video-containers row">
+          <?php /*set arguments */
 					$videosArgs = array(
 					    'posts_per_page' 	=> 3,
 					    'category_name' 	=> NULL, //reset
@@ -60,15 +75,15 @@
 						))*/
 					);
 				?>
-				<?php bi_display_popular_videos($videosArgs);?>
-
-			</div>
-			
-		</div>
-		<div class="col-md-6">
-			<h3><?php ucwords(single_cat_title());?> Products</h3>			
-			<div class="category-video-containers row">
-			<?php /*set arguments */
+          <?php bi_display_popular_videos($videosArgs);?>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <h3>
+          <?php ucwords(single_cat_title());?>
+          Products</h3>
+        <div class="category-video-containers row">
+          <?php /*set arguments */
 				$productArgs = array(
 				    'posts_per_page' 	=> 10,
 					'post_type'			=> 'product',			    
@@ -84,11 +99,12 @@
 					)) */
 				);
 			?>
-			<div class="category-product-container slick-slider-four" id="products-carousel">
-				<?php bi_display_products($productArgs); ?>
-			</div>
-		</div>
-	</div>
+          <div class="category-product-container slick-slider-four" id="products-carousel">
+            <?php bi_display_products($productArgs); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
 <?php get_footer(); ?>
