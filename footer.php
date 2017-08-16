ad084c293f65c5aa38355baba961f087428d4807
Wingedge <moreno.francis@gmail.com>
2017-08-16 15:45:48 +0800

    fix footer layotus

--- footer.php
+++ footer.php
@@ -1,37 +1,37 @@
 <div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="our-partners">        
+  <div class="row">   
+    <div class="our-partners">        
+      <div class="col-md-12">
         <h3 class="pink-dashed"><span>Our Partners</span></h3>
-        <div id="our_partners" class="text-center"> 
-          <div class="col-md-2 col-xs-6 col-sm-4">        
-            <a href="http://justrunlah.com/" target="_blank" rel="noopener"> 
-              <img src="/wp-content/uploads/2016/04/jrl-logo.png" class="img-responsive" alt="just Ran Lah"> 
-            </a>
-          </div>
-          <div class="col-md-2 col-xs-6 col-sm-4">        
-        <a href="http://www.missuniversesingapore.com.sg/" target="_blank" rel="noopener"> 
-          <img src="/wp-content/uploads/2016/04/MUS-2015-Logo-pink.png" class="img-responsive"  alt="Mus">
-        </a>
+      </div>
+      <div id="our_partners" class="text-center"> 
+        <div class="col-md-2 col-xs-6 col-sm-4">        
+          <a href="http://justrunlah.com/" target="_blank" rel="noopener"> 
+            <img src="/wp-content/uploads/2016/04/jrl-logo.png" class="img-responsive" alt="just Ran Lah"> 
+          </a>
         </div>
         <div class="col-md-2 col-xs-6 col-sm-4">        
-        <a href="https://www.facebook.com/MissSingaporeBeautyPageant/" target="_blank" rel="noopener">
-          <img src="/wp-content/uploads/2016/06/miss-singapore-beauty-pageant.png" class="img-responsive"  alt="miss singapore">
-        </a>
-        </div>
-        <div class="col-md-3 col-xs-6 col-sm-6">        
-        <a href="http://www.spaandwellness.org/" target="_blank" rel="noopener">
-          <img src="/wp-content/uploads/2016/04/swas-logo.png" class="img-responsive" alt="Spa &amp; Wellness">
-        </a>
-        </div>
-        <div class="col-md-3 col-xs-6 col-sm-6">        
-        <a href="http://www.cosmoprof.com.sg/" target="_blank" rel="noopener">
-          <img src="/wp-content/uploads/2016/04/cosmo-prof-acad-web.png" class="img-responsive"  alt="Cosnoprof">
-        </a>
-        </div>
-        </div>
+      <a href="http://www.missuniversesingapore.com.sg/" target="_blank" rel="noopener"> 
+        <img src="/wp-content/uploads/2016/04/MUS-2015-Logo-pink.png" class="img-responsive"  alt="Mus">
+      </a>
       </div>
-    </div>
+      <div class="col-md-2 col-xs-6 col-sm-4">        
+      <a href="https://www.facebook.com/MissSingaporeBeautyPageant/" target="_blank" rel="noopener">
+        <img src="/wp-content/uploads/2016/06/miss-singapore-beauty-pageant.png" class="img-responsive"  alt="miss singapore">
+      </a>
+      </div>
+      <div class="col-md-3 col-xs-6 col-sm-6">        
+      <a href="http://www.spaandwellness.org/" target="_blank" rel="noopener">
+        <img src="/wp-content/uploads/2016/04/swas-logo.png" class="img-responsive" alt="Spa &amp; Wellness">
+      </a>
+      </div>
+      <div class="col-md-3 col-xs-12 col-sm-6">        
+      <a href="http://www.cosmoprof.com.sg/" target="_blank" rel="noopener">
+        <img src="/wp-content/uploads/2016/04/cosmo-prof-acad-web.png" class="img-responsive"  alt="Cosnoprof">
+      </a>
+      </div>
+      </div>
+    </div>   
   </div>
 </div>
 <div class="site-footer">
