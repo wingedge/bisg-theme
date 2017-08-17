3f89a7d1403549722cb3f06758125fe6f587961e
Wingedge <moreno.francis@gmail.com>
2017-08-17 21:42:17 +0800

    added layout for products

--- category-general.php
+++ category-general.php
@@ -6,7 +6,7 @@
 	<div class="category-content container">
 		<div class="row">
 			<div class="col-md-9">
-				<h3 class="cat-titles pink-dashed"><span><?php ucwords(single_cat_title());?></span></h3>
+				<h3 class="cat-titles pink-dashed"><span><?php ucwords(single_cat_title());?> Products</span></h3>
 				<div class="category-product-container product-container">
 					<?php 
 						
@@ -20,14 +20,51 @@
 						$productArgs = array(
 				    		'posts_per_page' 	=> -1,
 							'post_type'			=> 'product',			    
-				    		'category_name' 	=> NULL, //reset
-				    		'file_template'	 	=> 'section/category-product.php',		
-				    		'column_width'		=> 'col-md-3',
+				    		'category_name' 	=> NULL, //reset			    		
 				    		'paged'				=> $paged,		    		
-				    		'cat' 				=> $catId,
+				    		'tax_query' 		=> array( array(
+									            'taxonomy' => 'category',
+									            'field' => 'slug',
+									            'terms' => $catSlug,
+									            //'operator' => 'AND'									           
+							)) 	
 						);
-					?>              
-                	<?php bi_display_products($productArgs); ?>					
+					    
+                	
+						$query = new WP_Query( $productArgs );	
+						$postCtr=1;
+					?>
+					
+					<?php if ($query->have_posts()) : ?>
+    					<div class="row category-row">
+    					<?php while ($query->have_posts()) :?>       	
+        					<?php $query->the_post(); ?>
+							<div class="col-md-4">
+								<div id="post-<?php the_ID(); ?>" <?php post_class('featured-products'); ?> >
+									<a href="<?php the_permalink();?>" title="<?php the_title();?>">
+									<?php if ( !has_post_thumbnail() ): ?>
+										<?php echo bi_get_post_image();?>
+									<?php else:?>
+										<?php the_post_thumbnail();?>	
+									<?php endif;?>
+										<div class="featured-products-title">
+											<span class="fp-title"><?php the_title();?></span>
+											<span class="icon-review"><i class="fa fa-check-square-o" aria-hidden="true"></i> Review</span>						
+										</div>
+									</a>
+								</div>
+							</div>
+							<?php if($postCtr>=3): $postCtr=0;?>								
+								</div><div class="row category-row">
+							<?php endif;?>
+							
+
+        					<?php $postCtr++; ?>
+    					<?php endwhile;?>
+    					</div>
+    					<?php wp_reset_postdata();?>
+					<?php endif;?>
+					
                 </div>
 			</div>
 			<div id="all-articles" class="all-articles col-md-3">
