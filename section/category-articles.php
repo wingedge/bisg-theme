524d4701ce8033e152fc5e1ceda7ab1520b0f641
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-17 13:40:34 +0800

    styling category page
    styling category page

--- section/category-articles.php
+++ section/category-articles.php
@@ -1,4 +1,5 @@
-<div class="row recent-article-row recent-article-row-<?php echo $postCtr;?>"><div>
+<div class="row recent-article-row recent-article-row-<?php echo $postCtr;?>">
+<div>
   <div id="post-<?php the_ID(); ?>" <?php post_class('recent-article-post'); ?>>
     <div class="col-md-4 recent-article-thumb"> <a href="<?php the_permalink();?>" title="<?php the_title();?>">
       <?php if ( !has_post_thumbnail() ): ?>
@@ -6,9 +7,12 @@
       <?php else:?>
       <?php the_post_thumbnail();?>
       <?php endif;?>
-      </a> </div>
+      </a>
+    </div>
     <div class="col-md-8 recent-article-title"> <a href="<?php the_permalink();?>" title="<?php the_title();?>"> <span class="fp-title">
       <?php the_title();?>
-      </span> </a> </div>
+      </span> </a>
+    </div>
   </div>
-</div><div>
+</div>
+<div>
