02ce9550f0b75ef3c6092a67f85bcb36df344a4a
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-17 14:57:00 +0800

    fixing category layout

--- section/catpage-category-articles.php
+++ section/catpage-category-articles.php
@@ -1,18 +1,16 @@
 <div class="col-md-4 recent-article-row-<?php echo $postCtr;?>">
   <div id="post-<?php the_ID(); ?>" <?php post_class('recent-article-post'); ?>>
     <div class="row">
-    <div class="col-md-12 recent-article-thumb"> <a href="<?php the_permalink();?>" title="<?php the_title();?>">
-      <?php if ( !has_post_thumbnail() ): ?>
-      <?php echo bi_get_post_image();?>
-      <?php else:?>
-      <?php the_post_thumbnail();?>
-      <?php endif;?>
-      </a>
-    </div>
-    <div class="col-md-12 recent-article-title"> <a href="<?php the_permalink();?>" title="<?php the_title();?>"> <span class="fp-title">
-      <?php the_title();?>
-      </span> </a>
-    </div>
+      <div class="col-md-12 recent-article-thumb"> <a href="<?php the_permalink();?>" title="<?php the_title();?>">
+        <?php if ( !has_post_thumbnail() ): ?>
+        <?php echo bi_get_post_image();?>
+        <?php else:?>
+        <?php the_post_thumbnail();?>
+        <?php endif;?>
+        </a> </div>
+      <div class="col-md-12 recent-article-title"> <a href="<?php the_permalink();?>" title="<?php the_title();?>"> <span class="fp-title">
+        <?php the_title();?>
+        </span> </a> </div>
     </div>
   </div>
 </div>
