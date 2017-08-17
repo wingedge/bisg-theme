6a9f48a924daa658f5935a68f0ea21bf43a26936
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-17 13:42:00 +0800

    styling category page

--- section/category-articles.php
+++ section/category-articles.php
@@ -1,5 +1,6 @@
 <div class="row recent-article-row recent-article-row-<?php echo $postCtr;?>">
   <div id="post-<?php the_ID(); ?>" <?php post_class('recent-article-post'); ?>>
+    <div>
     <div class="col-md-4 recent-article-thumb"> <a href="<?php the_permalink();?>" title="<?php the_title();?>">
       <?php if ( !has_post_thumbnail() ): ?>
       <?php echo bi_get_post_image();?>
@@ -11,6 +12,7 @@
     <div class="col-md-8 recent-article-title"> <a href="<?php the_permalink();?>" title="<?php the_title();?>"> <span class="fp-title">
       <?php the_title();?>
       </span> </a>
+    </div>
     </div>
   </div>
 </div>
