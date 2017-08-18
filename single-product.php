4773a58cb984a5572c0b58310c0fa6e9dccbb95f
Wingedge <moreno.francis@gmail.com>
2017-08-18 17:53:35 +0800

    adding attributes

--- single-product.php
+++ single-product.php
@@ -7,13 +7,6 @@
       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <div class="row">
           <div class="col-md-12">
-            <div class="content-image-banner">
-              <?php the_post_thumbnail();?>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
             <div class="content-title">
               <h2>
                 <?php the_title();?>
@@ -21,13 +14,42 @@
             </div>
           </div>
         </div>
+
         <div class="row">
-          <div class="col-md-12">
-            <div class="content-categories"> <strong>Categories : </strong>
-              <?php the_category(); ?>
+          
+          <div class="col-md-6">
+            <div class="content-image-banner">
+              <?php the_post_thumbnail();?>
             </div>
           </div>
+       
+          <div class="col-md-6">
+            <div class="product-attributes"> 
+
+              <?php $attributes = get_the_terms(get_the_id(),'attribute_category'); ?>
+              <?php if($attributes):?>                                  
+                <?php foreach($attributes as $attribute):?>
+                  <?php if($attribute->parent > 0):?>
+                    <span class="attribute btn btn-default btn-sm"><?php echo $attribute->name;?></span>
+                  <?php endif;?>
+                <?php endforeach;?>
+              <?php endif;?>
+
+              <?php $attributes = get_the_terms(get_the_id(),'category'); ?>
+              <?php if($attributes):?>                                  
+                <?php foreach($attributes as $attribute):?>
+                  <?php if($attribute->parent > 0):?>
+                    <span class="attribute btn btn-default btn-sm"><?php echo $attribute->name;?></span>
+                  <?php endif;?>
+                <?php endforeach;?>
+              <?php endif;?>
+
+
+            </div>
+          </div>
+
         </div>
+
         <div class="row">
           <div class="col-md-12">
             <div class="entry-content">
@@ -44,7 +66,7 @@
           <h3 class="cat-titles"><span>Recent Blogs</span></h3>
         </div>
       </div>
-      <?php get_sidebar('article');?>
+      <?php get_sidebar('articles');?>
     </div>
   </div>
 </div>
