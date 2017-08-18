b0e0a6cce076303753a26b70867bd6129b03b66d
Wingedge <moreno.francis@gmail.com>
2017-08-18 14:16:06 +0800

    added mobile adjustment for sidebar articles

--- section/category-articles.php
+++ section/category-articles.php
@@ -1,7 +1,7 @@
 <div class="row recent-article-row recent-article-row-<?php echo $postCtr;?>">
   <div id="post-<?php the_ID(); ?>" <?php post_class('recent-article-post'); ?>>
     <div>
-    <div class="col-md-4 recent-article-thumb"> <a href="<?php the_permalink();?>" title="<?php the_title();?>">
+    <div class="col-md-4 col-sm-3 recent-article-thumb"> <a href="<?php the_permalink();?>" title="<?php the_title();?>">
       <?php if ( !has_post_thumbnail() ): ?>
       <?php echo bi_get_post_image();?>
       <?php else:?>
@@ -9,7 +9,7 @@
       <?php endif;?>
       </a>
     </div>
-    <div class="col-md-8 recent-article-title"> <a href="<?php the_permalink();?>" title="<?php the_title();?>"> <span class="fp-title">
+    <div class="col-md-8 col-sm-9 recent-article-title"> <a href="<?php the_permalink();?>" title="<?php the_title();?>"> <span class="fp-title">
       <?php the_title();?>
       </span> </a>
     </div>
