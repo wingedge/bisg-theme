19e105dcd88e21ae4849ab2645c1eef0bb9e5a10
Wingedge <moreno.francis@gmail.com>
2017-08-18 19:16:56 +0800

    added review form

---
+++ section/review-form.php
@@ -0,0 +1,8 @@
+<div class="col-md-12">
+	<form>
+		<h4>Write a Review</h4>
+		<input type="text" class="form-control" placeholder="">
+		<textarea class="form-control" rows="3"></textarea>
+
+	</form>
+</div>
--- single-product.php
+++ single-product.php
@@ -54,6 +54,7 @@
             <div class="row">
               <div class="col-md-12">
                 <div class="entry-content">
+                  <h4>Product Description</h4>
                   <?php the_content(); ?>
                 </div>
               </div>
@@ -61,6 +62,11 @@
           </div>
         
         </div>
+
+        <div class="row">
+          <?php get_template_part('section/review','form');?>
+        </div>
+
       </div>
       <?php endwhile; // End the loop. Whew. ?>
 
