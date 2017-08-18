0bc596ad3da402da2da3f39c1946b74d2028419f
Wingedge <moreno.francis@gmail.com>
2017-08-18 19:25:45 +0800

    added ratings pre function

--- section/review-form.php
+++ section/review-form.php
@@ -1,8 +1,31 @@
-<div class="col-md-12">
-	<form>
-		<h4>Write a Review</h4>
-		<input type="text" class="form-control" placeholder="">
-		<textarea class="form-control" rows="3"></textarea>
+<div class="col-md-12">	
+	<h4>Write a Review</h4>		
+	<form class="form-horizontal">
+		<div class="form-group">
+			<label for="text1" class="col-sm-2 control-label">Title</label>
+			<div class="col-sm-10">
+			  <input type="email" class="form-control" id="text1" placeholder="Email">
+			</div>
+		</div>
 
+		<div class="form-group">
+			<label for="text2" class="col-sm-2 control-label">Rating</label>
+			<div class="col-sm-10">
+				<div id="hearts-existing" class="starrr" data-rating='4'></div>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="text2" class="col-sm-2 control-label">Review</label>
+			<div class="col-sm-10">
+			  <textarea class="form-control" id="text2" rows="3"></textarea>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+			  <button type="submit" class="btn btn-warning">Submit Review</button>
+			</div>
+		</div>
 	</form>
 </div>
