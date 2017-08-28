7610cf6da1406891d3da0a33d4769954119bd635
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-29 04:09:43 +0800

    adding sidebar post on pages

--- page.php
+++ page.php
@@ -1,25 +1,29 @@
 <?php get_header(); ?>
 <?php get_template_part('section/breadcrumbs'); ?>
+
 <div class="main-content container sub-page">
-	<div class="row">
-		<div id="main" class="main-column col-md-9">		
-			<?php while ( have_posts() ) : the_post(); ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="entry-content">					
-						<h2 class="content-title"><?php the_title();?></h2>
-						<hr class="divider"/>
-						<?php the_content(); ?>
-					</div>
-				</div>
-			<?php endwhile; // End the loop. Whew. ?>
-		</div>
-        <div id="all-articles" class="all-articles col-md-3">
-        <div>
-          <h3 class="cat-titles"><span>Recent Posts</span></h3>
-          <?php get_sidebar('category');?>
+  <div class="row">
+    <div id="main" class="main-column col-md-9">
+      <?php while ( have_posts() ) : the_post(); ?>
+      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="entry-content">
+          <h2 class="content-title">
+            <?php the_title();?>
+          </h2>
+          <hr class="divider"/>
+          <?php the_content(); ?>
         </div>
       </div>
-	</div>
+      <?php endwhile; // End the loop. Whew. ?>
+    </div>
+    <div class="ategory-page">
+    <div id="all-articles" class="all-articles col-md-3">
+      <div>
+        <h3 class="cat-titles"><span>Recent Posts</span></h3>
+        <?php get_sidebar('category');?>
+      </div>
+    </div>
+    </div>
+  </div>
 </div>
-
 <?php get_footer(); ?>
