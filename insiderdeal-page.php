41e5765a99323bcb6a01c5084735ae93f7d3a47f
CristalAbelgas <cristalabelgas@gmail.com>
2017-08-29 04:27:07 +0800

    adding insider template

---
+++ insiderdeal-page.php
@@ -0,0 +1,37 @@
+<?php 
+/* Template name: Insider Deals */
+get_header(); ?>
+<?php get_template_part('section/breadcrumbs'); ?>
+
+<div class="main-content container sub-page">
+  <div class="row">
+    <div id="main" class="main-column col-md-9">
+      <?php while ( have_posts() ) : the_post(); ?>
+      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="entry-content">
+          <h2 class="content-title">
+            <?php the_title();?>
+          </h2>
+          <hr class="divider"/>
+          <span class="insider-title"><?php the_field('title');?></span>
+          <a href="<?php the_field('promo_link');?>"><img src="<?php the_field('image');?>" alt=""/></a>
+          <div class="insider-promo">
+          	<h3>GRAB THIS DEAL!</h3>
+            <a href="<?php the_field('promo_link');?>"><?php the_field('promo');?></a>
+          </div>
+          <?php the_content(); ?>
+        </div>
+      </div>
+      <?php endwhile; // End the loop. Whew. ?>
+    </div>
+    <div class="category-page">
+    <div id="all-articles" class="all-articles col-md-3">
+      <div>
+        <h3 class="cat-titles"><span>Recent Posts</span></h3>
+        <?php get_sidebar('category');?>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
