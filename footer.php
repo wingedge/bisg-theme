a321e0b4eb1eec96a66e766a388398f31e25726f
Wingedge <moreno.francis@gmail.com>
2017-09-08 14:13:48 +0800

    footer fix

--- footer.php
+++ footer.php
@@ -7,27 +7,27 @@
       <div id="our_partners" class="text-center"> 
         <div class="col-md-2 col-xs-6 col-sm-4">        
           <a href="http://justrunlah.com/" target="_blank" rel="noopener"> 
-            <img src="/wp-content/uploads/2016/04/jrl-logo.png" class="img-responsive" alt="just Ran Lah"> 
+            <img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/04/jrl-logo.png" class="img-responsive" alt="just Ran Lah"> 
           </a>
         </div>
         <div class="col-md-2 col-xs-6 col-sm-4">        
       <a href="http://www.missuniversesingapore.com.sg/" target="_blank" rel="noopener"> 
-        <img src="/wp-content/uploads/2016/04/MUS-2015-Logo-pink.png" class="img-responsive"  alt="Mus">
+        <img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/04/MUS-2015-Logo-pink.png" class="img-responsive"  alt="Mus">
       </a>
       </div>
       <div class="col-md-2 col-xs-6 col-sm-4">        
       <a href="https://www.facebook.com/MissSingaporeBeautyPageant/" target="_blank" rel="noopener">
-        <img src="/wp-content/uploads/2016/06/miss-singapore-beauty-pageant.png" class="img-responsive"  alt="miss singapore">
+        <img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/06/miss-singapore-beauty-pageant.png" class="img-responsive"  alt="miss singapore">
       </a>
       </div>
       <div class="col-md-3 col-xs-6 col-sm-6">        
       <a href="http://www.spaandwellness.org/" target="_blank" rel="noopener">
-        <img src="/wp-content/uploads/2016/04/swas-logo.png" class="img-responsive" alt="Spa &amp; Wellness">
+        <img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/04/swas-logo.png" class="img-responsive" alt="Spa &amp; Wellness">
       </a>
       </div>
       <div class="col-md-3 col-xs-12 col-sm-6">        
       <a href="http://www.cosmoprof.com.sg/" target="_blank" rel="noopener">
-        <img src="/wp-content/uploads/2016/04/cosmo-prof-acad-web.png" class="img-responsive"  alt="Cosnoprof">
+        <img src="<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2016/04/cosmo-prof-acad-web.png" class="img-responsive"  alt="Cosnoprof">
       </a>
       </div>
       </div>
@@ -40,7 +40,7 @@
       <div class="col-md-3 col-sm-3 footerbox">
         <h3 class="footer-title">Connect with us!</h3>
        	<ul>
-        	<li class="biph"><a href="#"><span class="phicon"></span><span class="icon-title">BeautyInsider PH</span></a></li>
+        	<li class="biph"><a href="http://www.beautyinsider.ph"><span class="phicon"></span><span class="icon-title">BeautyInsider PH</span></a></li>
             <li><a href="#"><span class="socialmediaicon icon-fb"></span><span class="icon-title">Facebook</span></a></li>
             <li><a href="#"><span class="socialmediaicon icon-instagram"></span><span class="icon-title">Instagram</span></a></li>
             <li><a href="#"><span class="socialmediaicon icon-youtube"></span><span class="icon-title">Youtube</span></a></li>
@@ -58,16 +58,16 @@
       <div class="col-md-3 col-sm-5 footerbox">
         <h3 class="footer-title">Quick Links</h3>
         <ul>
-        	<li><a href="#">Rewards and Redemption</a></li>
-            <li><a href="#">Contributors</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Trial Team Form</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Hey Gorgeous Bag</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Beauty Influencers</a></li>
-            <li><a href="#">Terms & Conditions</a></li>
-            <li><a href="#">How to Claim your Rewards</a></li>
+        	<li><a href="<?php echo site_url('/rewards-and-redemption/');?>">Rewards and Redemption</a></li>
+            <li><a href="<?php echo site_url('contributors');?>">Contributors</a></li>
+            <li><a href="<?php echo site_url('faqs');?>">FAQs</a></li>
+            <li><a href="<?php echo site_url('trial-team-form');?>">Trial Team Form</a></li>
+            <li><a href="<?php echo site_url('about-us');?>">About Us</a></li>
+            <li><a href="<?php echo site_url('hey-gorgeous');?>">Hey Gorgeous Bag</a></li>
+            <li><a href="<?php echo site_url('contact-us');?>">Contact Us</a></li>
+            <li><a href="<?php echo site_url('beauty-influencers');?>">Beauty Influencers</a></li>
+            <li><a href="<?php echo site_url('terms-conditions');?>">Terms & Conditions</a></li>
+            <li><a href="<?php echo site_url('how-to-claim-your-rewards');?>">How to Claim your Rewards</a></li>
         </ul>
       </div>
       <div class="col-md-3 col-sm-12 footerbox">        
@@ -76,7 +76,7 @@
           <input type="text" value="enter your email addess" />
           <input name="" type="button" value="subscribe now" />
           </form>
-          <h3 class="footer-sub-title">Advertise with us <a href="mailto:sean@mapletreemedia.com">Email </a> or fill out the form <a href="http://beautyinsider.sg/advertise-with-us/">here</a></h3>
+          <h3 class="footer-sub-title">Advertise with us <a href="mailto:sean@mapletreemedia.com">Email </a> or fill out the form <a href="<?php echo site_url('advertise-with-us');?>">here</a></h3>
         </div>
       </div>
     </div>
