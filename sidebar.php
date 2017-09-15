afd8d7397c37fd33269fc9f9a4e8a919f776b634
Wingedge <moreno.francis@gmail.com>
2017-09-15 17:11:27 +0800

    fixing sidebar layout 768px

--- sidebar.php
+++ sidebar.php
@@ -7,37 +7,31 @@
 <?php else:?>
 <?php endif; // end primary widget area ?>
 
-<div class="row">
+<div class="row row-sidebar-wrap">
+
   <div class="col-md-12 col-sm-12 s-widget">
     <div class="find-exact-what-you-need">
       <h1>Find Exactly What You Need!</h1>
     </div>
   </div>
-</div>
 
-<div class="row">
-  <div class="col-md-12 col-sm-6 recent-review s-widget">
+  <div class="col-md-12 col-sm-4 recent-review s-widget">
       <?php 
         $review = new BIReviewer();
         $review->render_random_review();
       ?>    
   </div>
-</div>
 
-<div class="row">
-  <div class="col-md-12 col-sm-6 s-widget">    
+  <div class="col-md-12 col-sm-4 s-widget">    
     <img src="<?php bloginfo( 'template_url' ); ?>/img/Travel-Insider-Banner.png"/> 
   </div>
-</div>
 
-<div class="row">
-  <div class="col-md-12 col-sm-6 s-widget">
+
+  <div class="col-md-12 col-sm-4 s-widget">
     <img src="<?php bloginfo( 'template_url' ); ?>/img/Award-winners-1.jpeg"/>
-  </div>
-</div>
+  </div> 
 
-<div class="row">
-  <div class="col-md-12 col-sm-6 s-featured-video s-widget">
+  <div class="col-md-12 col-sm-4 s-featured-video s-widget">
     <div class="featured-video-container slick-slider-one" id="sidebar-product-carousel">
       <?php $sidebarArgs = array(          
           'posts_per_page'  => 4,
@@ -51,12 +45,11 @@
       <?php bi_display_products($sidebarArgs);?>
     </div>
   </div>
-</div>
 
-<div class="row">
-  <div class="col-md-12 col-sm-6 s-widget">
+  <div class="col-md-12 col-sm-4 s-widget">
     <img src="<?php bloginfo( 'template_url' ); ?>/img/BI-membership-banner-awards-280x505.jpg" class="img-responsive"/>
   </div>
+
 </div>
 
 
