a75d8677ca8acaa519d4a1f1a6740f96e807e26a
CristalAbelgas <cristalabelgas@gmail.com>
2017-09-20 23:05:40 +0800

    fixing brand post

--- section/section-brands.php
+++ section/section-brands.php
@@ -1,58 +1,74 @@
+<style>
+.brand_map iframe {
+	max-width:100%;
+}
+#tabs-single-product .tab-pane > div {
+	padding: 25px;
+}
+.content-title.brand-title {
+  display: block;
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+.content-title.brand-title hr {
+  margin: 15px 0 0;
+  padding: 0;
+}
+.tabbable.bi-tabs {
+  margin-top: 25px;
+}
+</style>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="row">   
-          <div class="col-md-4">
-            <div class="content-image-banner">
-              <?php the_post_thumbnail();?>
-            </div>
-          </div>     
-          <div class="col-md-8">
-            <div class="row">
-              <div class="col-md-12">       
-                <div class="entry-content">         
-                  
-
-                  <h2 class="content-title">About <?php the_title();?></h2>
-                 
-                  <?php echo get_the_tag_list('<p>Tags: ',', ','</p>'); ?>
-                  <?php the_content();?>
-                </div>        
-              </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="content-title brand-title">About -
+        <?php the_title();?><hr/>
+      </h2>
+    </div>
+  </div>
+  <div class="row" style="padding-top:25px;">
+    <div class="col-md-4 brand_img_thumb">
+      <?php the_post_thumbnail('full');?>
+    </div>
+    <div class="col-md-8 brand_content">
+      <div class="brand_tag_list"><?php echo get_the_tag_list('<p>Tags: ',', ','</p>'); ?></div>
+      <div class="brand_content_wrap">
+        <?php the_field('brand_content');?>
+      </div>
+      <?php //the_content();?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="tabbable  bi-tabs" id="tabs-single-product">
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#panel-contact" data-toggle="tab">Contact Information</a></li>
+          <!--<li><a href="#panel-description" data-toggle="tab">Description</a></li>-->
+          <li><a href="#panel-maps" data-toggle="tab">Maps</a></li>
+          <li><a href="#panel-products" data-toggle="tab">Products</a></li>
+          <li><a href="#panel-videos" data-toggle="tab">Videos</a></li>
+          <li><a href="#panel-reviews" data-toggle="tab">Reviews</a></li>
+        </ul>
+        <div class="tab-content">
+          <div class="tab-pane active" id="panel-contact">
+            <div>
+              <?php the_field('brand_info'); ?>
             </div>
           </div>
+          <div class="tab-pane" id="panel-maps">
+            <div><?php echo html_entity_decode( get_field('brand_map'));?></div>
+          </div>
+          <div class="tab-pane" id="panel-products">
+            <div>...</div>
+          </div>
+          <div class="tab-pane" id="panel-videos">
+            <div><?php echo html_entity_decode( get_field('brand_video'));?></div>
+          </div>
+          <div class="tab-pane" id="panel-reviews">
+            <div>...</div>
+          </div>
         </div>
-        
-        <div class="row" style="padding:15px 0;">   
-          <div class="col-md-12">
-            <div class="tabbable  bi-tabs" id="tabs-single-product">           
-              
-              <ul class="nav nav-tabs">
-                <li class="active"><a href="#panel-contact" data-toggle="tab">Contact Information</a></li>
-                <!--<li><a href="#panel-description" data-toggle="tab">Description</a></li>-->
-                <li><a href="#panel-maps" data-toggle="tab">Maps</a></li>
-                <li><a href="#panel-products" data-toggle="tab">Products</a></li>
-                <li><a href="#panel-videos" data-toggle="tab">Videos</a></li>
-                <li><a href="#panel-reviews" data-toggle="tab">Reviews</a></li>
-              </ul>           
-
-              <div class="tab-content">                
-                <div class="tab-pane active" id="panel-contact">                                 
-                  <?php the_field('contact_information'); ?>                                    
-                </div>                
-                <div class="tab-pane" id="panel-maps">                  
-                  <?php echo html_entity_decode( get_field('maps'));?>
-                </div>
-                <div class="tab-pane" id="panel-products">                  
-                  
-                </div>
-                <div class="tab-pane" id="panel-videos">                  
-                  
-                </div>
-                <div class="tab-pane" id="panel-reviews">                  
-                  
-                </div>              
-              </div>
-
-            </div>
-          </div>          
-        </div>
-      </div>   
+      </div>
+    </div>
+  </div>
+</div>
--- single-brand.php
+++ single-brand.php
@@ -1,26 +1,21 @@
 <?php get_header(); ?>
 <?php get_template_part('section/breadcrumbs'); ?>
-<div class="main-content single-product-wrap">
-  <div class="row">
-    <div id="main" class="main-column brand-column">
-      <?php while ( have_posts() ) : the_post(); ?>
-      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>               
-      
-      <?php if( has_term('Featured', 'brand-category') ):?>      
-        <div class="entry-content brand-container">
-        <?php the_content();?>  
-        </div>      
-      <?php else:?>
-        <div class="entry-content container">
-        
-          <?php get_template_part('section/section','brands');?>
 
-        </div>      
-      <?php endif;?>                  
-      
-      </div>      
+<div class="main-content single-product-wrap">
+  <div id="main" class="main-column brand-column">
+    <?php while ( have_posts() ) : the_post(); ?>
+    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <?php if( has_term('Featured', 'brand-category') ):?>
+      <div class="entry-content brand-container">
+        <?php the_content();?>
+      </div>
+      <?php else:?>
+      <div class="entry-content container">
+        <?php get_template_part('section/section','brands');?>
+      </div>
+      <?php endif;?>
+    </div>
     <?php endwhile; // End the loop. Whew. ?>
-    </div>    
   </div>
 </div>
 <?php get_footer(); ?>
