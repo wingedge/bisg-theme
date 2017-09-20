c437db3eb327f6fe125eba64c67cf3c6be36432c
CristalAbelgas <cristalabelgas@gmail.com>
2017-09-20 21:41:11 +0800

    fixing search product layuot

--- page-products.php
+++ page-products.php
@@ -29,18 +29,18 @@ get_header(); ?>
 
 					<div id="product-list">
 						<div class="row">
-							<div class="form-group col-md-10 col-sm-9 col-xs-7">											
+							<div class="form-group col-md-10 col-sm-9 col-xs-8">											
 								<input class="search form-control" placeholder="Find product..." />								
 							</div>
-							<div class="form-group col-md-2 col-sm-3  col-xs-4">			
-	  							<span class="sort btn btn-info" data-sort="name">Sort by Name</span>
+							<div class="form-group col-md-2 col-sm-3  col-xs-4 sortbtn-wrap">			
+	  							<span class="sort btn btn-info register-btn" data-sort="name">Sort by Name</span>
 	  						</div>
   						</div>
 
   						<!--<span class="sort" data-sort="city">Sort by city</span>-->											
 					
 					<?php if ($query->have_posts()) : $postCtr=1;?>
-    					<div class="scroller-box" style="max-height:300px; overflow:auto;">
+    					<div class="scroller-box" style="max-height:400px; overflow:auto;">
 	    					<ul class="list filter-list list-group">
 	    					<?php $currentLetter = '';?>
 	    					<?php while ($query->have_posts()) :?>
